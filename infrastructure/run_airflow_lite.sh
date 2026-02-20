3683edb4b874f22a7a46f8d07046df1911462fd5
EDWIN SIERRA <97609006+viejoedwinsierra@users.noreply.github.com>
2026-02-20 00:25:18 -0500

    Update run_airflow_lite.sh

--- infrastructure/run_airflow_lite.sh
+++ infrastructure/run_airflow_lite.sh
@@ -1,8 +1,15 @@
+#instalaar doker 
 sudo yum update -y
 sudo amazon-linux-extras install -y docker
 sudo systemctl enable --now docker
 docker --version || sudo docker --version
+#installar comp¿se
 
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.29.7/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose version
+
+#ir a directorio
 cd /home/ssm-user/airflow-docker
 
 cat > run_airflow_lite.sh <<'EOF'
